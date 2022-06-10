9b9b6ee5531199e0cfb2c481cba916afc5b5e6dc
kylejgoodwin <hi@kyle-goodwin.com>
2022-06-09 21:10:18 -0400

    Add api route for pickups

---
+++ backend/app/controllers/v1/pickups_controller.rb
@@ -0,0 +1,21 @@
+class V1::PickupsController < ApplicationController
+
+    def index
+        pickups = Pickup.where(organization_id: params[:organization_id])
+        render(json: pickups.as_json, status: :ok)
+    end 
+
+    def create
+        pickup = Pickup.create!(create_pickup_params)
+        render(json: pickup.as_json, status :ok)
+    end
+
+    private
+
+    def create_pickup_params
+        params.permit([:location_id, :datetime ])
+    end
+
+
+
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace "v1" do
-    resources :users, only: [:index], controller: "users"
+    resources :users, only: [:index]
     resources :locations, only: [:create]
     resources :pickups, only: [:create]
 
     resources :organization, only: [] do
-      resources :locations, only: [:index], controller: "locations"
+      resources :locations, only: [:index]
       resources :pickups, only: [:index]
     end
   end
